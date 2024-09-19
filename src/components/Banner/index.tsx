8376546743ffd3596b535fd40fc10b5856fc3da3
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-09-19 10:00:57 -0300

    feat: integrating Banner with our API

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,34 +1,46 @@
-import {Imagem, Title, Precos} from "./styles"
-import bannerImg from "../../assets/images/banner-homem-aranha.png"
-import Tag from "../Tag"
-import Button from "../Button"
+import { useEffect, useState } from 'react'
+import { Game } from '../../pages/Home'
+import Button from '../Button'
+import { formatPrice } from '../ProductsList'
+import Tag from '../Tag'
+import { Imagem, Precos, Title } from './styles'
 
-const Banner = () => 
-      <Imagem style={{backgroundImage: `url(${bannerImg})`}}>
-        <div className="wrapper">
-          
-          <Tag size="big">Destaque do dia</Tag>
-          <div>
-       <Title>
-        Marvel's Spider-Man: Miles Morales PS4 & PS5
-       </Title>
-       <Precos>
-       De <span>R$ 250,00</span><br /> por apenas R$ 99,90
-       </Precos>
-          </div>
-          <div>
-       <Button type="link" 
-       to="/produto"
-        title="Clique aqui e Aproveite a oferta" >
-        Aproveitar
-       </Button>
-          </div>
+const Banner = () => {
+  const [game, setGame] = useState<Game>()
+
+  useEffect(() => {
+    fetch('https://fake-api-tau.vercel.app/api/eplay/destaque')
+      .then((res) => res.json())
+      .then((res) => setGame(res))
+  }, [])
+
+  if (!game) {
+    return <h3>'carregando...'</h3>
+  }
+
+  return (
+    <Imagem style={{ backgroundImage: `url(${game.media.cover})` }}>
+      <div className="wrapper">
+        <Tag size="big">Destaque do dia</Tag>
+        <div>
+          <Title>{game.name}</Title>
+          <Precos>
+            De <span>{formatPrice(game.prices.old)}</span>
+            <br /> por apenas {formatPrice(game.prices.current)}
+          </Precos>
         </div>
-
-
-      </Imagem>
-         
-  
-
+        <div>
+          <Button
+            type="link"
+            to={`/produto/${game.id}`}
+            title="Clique aqui e Aproveite a oferta"
+          >
+            Aproveitar
+          </Button>
+        </div>
+      </div>
+    </Imagem>
+  )
+}
 
 export default Banner
--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,39 +1,48 @@
-import styled from "styled-components"
-import { TagContainer } from "../Tag/styles"
+import styled from 'styled-components'
+import { TagContainer } from '../Tag/styles'
 
-export const  Imagem = styled.div`
-width: 100%;
-height: 560px;
-background-repeat: no-repeat;
-background-size: cover;
-font-weight: 700;
-.wrapper {
-  padding-top: 340px;
+export const Imagem = styled.div`
+  width: 100%;
+  height: 560px;
+  display: block;
+  background-repeat: no-repeat;
+  background-size: cover;
+  font-weight: 700;
   position: relative;
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
-}
-${TagContainer} {
-  position: absolute;
-  top: 32px;
-}
+
+  .wrapper {
+    position: relative;
+    padding-top: 340px;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    z-index: 1;
+  }
+  ${TagContainer} {
+    position: absolute;
+    top: 32px;
+  }
+  &::after {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.8);
+    content: '';
+  }
 `
 export const Title = styled.h2`
-font-size: 36px;
-max-width: 456px;
-width: 100%;
-display: block;
-` 
+  font-size: 36px;
+  max-width: 456px;
+  width: 100%;
+  display: block;
+`
 
 export const Precos = styled.p`
-font-size: 24px;
-margin-top: 24px;
- span {
-  text-decoration: line-through;
- }
-
-
-
-
+  font-size: 24px;
+  margin-top: 24px;
+  span {
+    text-decoration: line-through;
+  }
 `
