9f185eb4aabd2209c95da5a42e97e9028a2cdefa
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-09-19 10:03:03 -0300

    chore: integrating the Product component into the API and updating the styles

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,14 +1,15 @@
-import Tag from "../Tag";
-import { CardProduct, TitleProduct, DescProduct, Infos } from "./styles";
+import Tag from '../Tag'
+import { CardProduct, DescProduct, Infos, TitleProduct } from './styles'
 
 type Props = {
-  title: string;
-  category: string;
-  system: string;
-  description: string;
-  infos: string[];
-  image: string;
-};
+  title: string
+  category: string
+  system: string
+  description: string
+  infos: string[]
+  image: string
+  id: number
+}
 
 const Product = ({
   title,
@@ -17,9 +18,16 @@ const Product = ({
   description,
   infos,
   image,
+  id
 }: Props) => {
+  const getDescription = (description: string) => {
+    if (description.length > 95) {
+      return description.slice(0, 92) + '...'
+    }
+    return description
+  }
   return (
-    <CardProduct>
+    <CardProduct to={`/produto/${id}`}>
       <img src={image} alt="{title}" />
       <Infos>
         {infos.map((info) => (
@@ -29,9 +37,9 @@ const Product = ({
       <TitleProduct>{title}</TitleProduct>
       <Tag>{category}</Tag>
       <Tag>{system}</Tag>
-      <DescProduct>{description}</DescProduct>
+      <DescProduct>{getDescription(description)}</DescProduct>
     </CardProduct>
-  );
-};
+  )
+}
 
-export default Product;
+export default Product
