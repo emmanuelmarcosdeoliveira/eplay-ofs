917a591a16e7c631ca1b373a461f7770ffb5dede
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-09-27 11:46:51 -0300

    chore:  feat requests have been replaced by requests built from redux-toolkit-query: promotions, coming soon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,85 +1,6 @@
-import { useEffect, useState } from 'react'
 import Banner from '../../components/Banner'
 import ProductsList from '../../components/ProductsList'
-
-// const promocoes: Game[] = [
-//   {
-//     id: 1,
-//     category: 'Ação',
-//     description: 'REsident Evil 4 ',
-//     title: 'Resident Evil',
-//     system: 'Windows',
-//     infos: ['10%', 'R$ 250.00'],
-//     image: resident
-//   },
-//   {
-//     id: 2,
-//     category: 'Ação',
-//     description: 'REsident Evil 4 ',
-//     title: 'Resident Evil',
-//     system: 'PS5',
-//     infos: ['5%', 'R$ 290.00'],
-//     image: resident
-//   },
-//   {
-//     id: 3,
-//     category: 'Ação',
-//     description: 'REsident Evil 4 ',
-//     title: 'Resident Evil',
-//     system: 'Windows',
-//     infos: ['10%', 'R$ 250.00'],
-//     image: resident
-//   },
-//   {
-//     id: 4,
-//     category: 'Ação',
-//     description: 'REsident Evil 4 ',
-//     title: 'Resident Evil',
-//     system: 'Windows',
-//     infos: ['10%', 'R$ 250.00'],
-//     image: resident
-//   }
-// ]
-
-// const emBreve: Game[] = [
-//   {
-//     id: 5,
-//     category: 'RPG',
-//     description: 'El diablo',
-//     title: 'Diablo 4',
-//     system: 'windows',
-//     infos: ['17/05'],
-//     image: diablo
-//   },
-//   {
-//     id: 6,
-//     category: 'RPG',
-//     description: 'Zelda',
-//     title: 'Diablo 4',
-//     system: 'nintendo Switch',
-//     infos: ['17/05'],
-//     image: zelda
-//   },
-//   {
-//     id: 7,
-//     category: 'RPG',
-//     description: 'Strar Wars',
-//     title: 'StarWars',
-//     system: 'PS5',
-//     infos: ['17/05'],
-//     image: starWars
-//   },
-//   {
-//     id: 8,
-//     category: 'RPG',
-//     description: 'El diablo',
-//     title: 'Diablo 4',
-//     system: 'Windows',
-//     infos: ['17/05'],
-//     image: diablo
-//   }
-// ]
-
+import { useGetOnSaleQuery, useGetSoonQuery } from '../../services/api'
 export interface GaleryItem {
   type: 'image' | 'video'
   url: string
@@ -110,26 +31,22 @@ export type Game = {
 }
 
 const Home = () => {
-  const [promocoes, setPromocoes] = useState<Game[]>([])
-  const [emBreve, setEmBreve] = useState<Game[]>([])
-
-  useEffect(() => {
-    fetch('https://fake-api-tau.vercel.app/api/eplay/promocoes')
-      .then((res) => res.json())
-      .then((res) => setPromocoes(res))
-
-    fetch('https://fake-api-tau.vercel.app/api/eplay/em-breve')
-      .then((res) => res.json())
-      .then((res) => setEmBreve(res))
-  }, [])
-
-  return (
-    <>
-      <Banner />
-      <ProductsList games={promocoes} title="Promoçoes" background="primary" />
-      <ProductsList games={emBreve} title="Em Breve" background="black" />
-    </>
-  )
+  const { data: onSaleGames } = useGetOnSaleQuery()
+  const { data: soonGames } = useGetSoonQuery()
+  if (onSaleGames && soonGames) {
+    return (
+      <>
+        <Banner />
+        <ProductsList
+          games={onSaleGames}
+          title="Promoçoes"
+          background="primary"
+        />
+        <ProductsList games={soonGames} title="Em Breve" background="black" />
+      </>
+    )
+  }
+  return <h4>Carregando . . .</h4>
 }
 
 export default Home
