2a651f271fc73af557b571a45c4dc5ad1b38e3a6
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-09-27 10:49:55 -0300

    feat:  the initial configuration of redux-toolkit was done and the dependencies were installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { GlobalStyles } from '../styles/styles'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import MainRoutes from './routes'
-import Footer from './components/Footer'
+import { store } from './store'
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <GlobalStyles />
-        <div className="wrapper">
-          <Header />
-        </div>
-        <MainRoutes />
-        <Footer />
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <GlobalStyles />
+          <div className="wrapper">
+            <Header />
+          </div>
+          <MainRoutes />
+          <Footer />
+        </BrowserRouter>
+      </Provider>
     </>
   )
 }
