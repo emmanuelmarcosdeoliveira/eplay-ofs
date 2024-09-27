639533b8dc58a77ed73cd4828b01889ec4eb7d6a
Emmanuel Marcos de Oliveiera <emmanuelmarcosdeoliveira@gmail.com>
2024-09-27 11:44:45 -0300

    chore:  requests were created, coming soon, highlights, promotions

---
+++ src/services/api.ts
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { Game } from '../pages/Home'
+
+const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://fake-api-tau.vercel.app/api/eplay'
+  }),
+  endpoints: (builder) => ({
+    geatFeatureGame: builder.query<Game, void>({
+      query: () => 'destaque'
+    }),
+    getOnSale: builder.query<Game[], void>({
+      query: () => 'promocoes'
+    }),
+    getSoon: builder.query<Game[], void>({
+      query: () => 'em-breve'
+    })
+  })
+})
+export const { useGeatFeatureGameQuery, useGetOnSaleQuery, useGetSoonQuery } =
+  api
+export default api
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
+import api from '../services/api'
+import cartReducers from './reducers/cart'
 
 export const store = configureStore({
-  reducer: {}
+  reducer: {
+    cart: cartReducers,
+    [api.reducerPath]: api.reducer
+  },
+  middleware: (getDefaultMidddleware) =>
+    getDefaultMidddleware().concat(api.middleware)
 })
 
 export type RootReducer = ReturnType<typeof store.getState>
